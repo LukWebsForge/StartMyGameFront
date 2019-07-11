1cb7fdca20eddf5f47f9b07f2501667113ff0c40
LukBukkit <luk.bukkit@gmail.com>
2019-07-11 22:10:00 +0200

    Updates dependencies & 4-space indent

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,33 @@
 <template>
-  <div id="app">
-    <MainPage></MainPage>
-  </div>
+    <div id="app">
+        <MainPage></MainPage>
+    </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import MainPage from './components/MainPage.vue';
+    import { Component, Vue } from 'vue-property-decorator';
+    import MainPage from './components/MainPage.vue';
 
-@Component({
-  components: {
-    MainPage,
-  },
-})
-export default class App extends Vue {}
+    @Component({
+        components: {
+            MainPage,
+        },
+    })
+    export default class App extends Vue {
+    }
 </script>
 
 <style>
-  @import url('~nes.css/css/nes.min.css');
+    @import url('~nes.css/css/nes.min.css');
 
-  html, body, pre, code, kdb, smap {
-    font-family: 'Press Start 2P', cursive;
-    -webkit-font-smoothing : none;
-    font-smooth: never;
-  }
+    html, body, pre, code, kdb, smap {
+        font-family: 'Press Start 2P', cursive;
+        -webkit-font-smoothing: none;
+        font-smooth: never;
+    }
 
-  body {
-    padding: 0 3rem;
-    margin: 3rem;
-  }
+    body {
+        padding: 0 3rem;
+        margin: 3rem;
+    }
 </style>
